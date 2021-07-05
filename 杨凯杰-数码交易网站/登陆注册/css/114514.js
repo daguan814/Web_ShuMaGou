89905db56a9fea9a724511f7e5ec9e182a4d023e
Miku_daisuki <545059446@qq.com>
2021-07-05 12:12:34 +0800

    Default Changelist

--- 杨凯杰-数码交易网站/登陆注册/css/114514.js
+++ 杨凯杰-数码交易网站/登陆注册/css/114514.js
@@ -5,5 +5,5 @@ function login(){
 	var admin=$("admin").value;
 	var password=$("password").value;
 	if(admin=="114514"&&password==123)
-		alert("登录成功！");
+		Window.location.href="../登陆后/loginindex.html"
 }
